557dfea252213efde103ab1cfd64488c6a9c9a62
Ashleydiligwapo <ashleydiligwapo@gmail.com>
2024-11-03 02:18:03 +0800

    Updated

--- backend/index.js
+++ backend/index.js
@@ -12,13 +12,9 @@ import DonePurchasedRoute from "./Routes/DonePurchased.js";
 import FacultyRoute from "./Routes/FacultyRoute.js";
 import UserRoute from "./Routes/UsersRoute.js";
 import ProductRoute from "./Routes/Products.js";
-
-<<<<<<< HEAD
 import MessageRoute from "./Routes/MessageRoute.js";
 import { createServer } from "http";
 
-=======
->>>>>>> d17886601206db42920ed9879099a13f81d22792
 dotenv.config();
 mongoose
   .connect(process.env.MONGODB_URI)
@@ -29,18 +25,9 @@ mongoose
 
 const PORT = process.env.PORT || 4000;
 const app = express();
-<<<<<<< HEAD
 const httpServer = createServer(app);
-=======
-// app.use(
-//   cors({
-//     origin: ["http://localhost:5173"],
-//     methods: ["GET", "POST", "PUT", "DELETE"],
-//     credentials: true,
-//   })
-// );
+createServer(app);
 
->>>>>>> d17886601206db42920ed9879099a13f81d22792
 app.use(
   cors({
     origin: ["http://localhost:5173"],
