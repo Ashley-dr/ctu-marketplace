86571a8852c5226174b75fcddbdd891fa390dd5a
Ashleydiligwapo <ashleydiligwapo@gmail.com>
2024-11-05 00:25:58 +0800

    Update

--- backend/index.js
+++ backend/index.js
@@ -35,7 +35,7 @@ const app = express();
 // );
 app.use(
   cors({
-    origin: ["https://marketplace-ctu.onrender.com"],
+    origin: "https://cebutechmarketplace.com" || "http://localhost:5173",
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true,
   })
